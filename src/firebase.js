7cbaf16bbf614d3b5b02b52c31b77725feafb2f2
Orloxx23 <madorlix@gmail.com>
2023-08-19 10:10:49 -0500

    [UPDATE]: Firebase config and new dependency

--- src/firebase.js
+++ src/firebase.js
@@ -5,7 +5,7 @@ const firebaseConfig = {
   apiKey: process.env.FIREBASE_APIKEY,
   authDomain: process.env.FIREBASE_AUTHDOMAIN,
   projectId: process.env.FIREBASE_PROJECTID,
-  storageBucket: process.env.FIREBASE_STORAGEBUCKET,
+  storageBucket: "tubepirate-86210.appspot.com",
   messagingSenderId: process.env.FIREBASE_MESSAGINGSENDERID,
   appId: process.env.FIREBASE_APPID,
   measurementId: process.env.FIREBASE_MEASUREMENTID,
